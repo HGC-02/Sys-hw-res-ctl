fa95cb2e67a8a6ded4046b372ebad6e054be9150
HGC-02 <studenthiggins@gmail.com>
2026-06-27 17:17:34 +0800

    add gover contorll

--- ui/cpu/cpu_ui.sh
+++ ui/cpu/cpu_ui.sh
@@ -1,8 +1,9 @@
 
-FRUIT=$(dialog --output-fd 1 --menu "選擇模式：" 10 40 2 "1" "修改最大值" "2" "修改最小值")
+FRUIT=$(dialog --output-fd 1 --menu "選擇模式：" 10 40 2 "1" "修改最大值" "2" "修改最小值" "3" "切換頻率控制模式")
 
 pwd
 case $FRUIT in 
   1) bash ui/cpu/cpu_max.sh ;;
   2) bash ui/cpu/cpu_min.sh ;;
+  3) sudo bash ui/cpu/gpu_grover.sh
 esac
